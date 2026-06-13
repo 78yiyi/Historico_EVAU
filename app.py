626c1569bf33daf0d734c28003a3e4db3151e56d
78yiyi <lparra@lasallegrinon.es>
2026-06-13 12:41:13 +0200

    mat2

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ if opcion == "Nº Presentados por Año":
             df_totales = df.copy() # Si no hay columna materia, asumimos que todos los datos son válidos
         # --- LA LÍNEA MÁGICA QUE LO SOLUCIONA ---
         # Obligamos a que la columna de Presentados sea un número matemático real
-        df_totales['Presentados'] = pd.to_numeric(df_totales['Presentados'], errors='coerce')
-            
+        
+
         # Eliminamos filas sin año y ordenamos cronológicamente
         df_totales = df_totales.dropna(subset=['Curso'])
         df_totales = df_totales.sort_values(by='Curso')
